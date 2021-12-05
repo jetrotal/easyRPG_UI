9ddbd461e46227723117074f7d48191bb15510ab
jetrotal <45118493+jetrotal@users.noreply.github.com>
2021-12-05 17:47:01 -0300

    Update loadLocalText.js

--- scripts/js/loadLocalText.js
+++ scripts/js/loadLocalText.js
@@ -2,7 +2,11 @@ function loadLocalText(target) {
   var rawFile = new XMLHttpRequest();
   rawFile.open("GET", target, true);
   rawFile.onreadystatechange = function() {
-    if (rawFile.readyState === 4)
-      return rawFile.responseText;
+    if (rawFile.readyState === 4) {
+      var allText = rawFile.responseText;
+console.log(allText)
+return allText
+    }
+rawFile.send();
   }
 }
