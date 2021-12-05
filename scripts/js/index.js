21077dfb58bd44492f4b668051d5d100fe55e170
jetrotal <45118493+jetrotal@users.noreply.github.com>
2021-12-05 17:21:04 -0300

    Update index.js

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -1,4 +1,4 @@
 var qmlFile= Array.prototype.slice.apply(document.getElementsByTagName('script')).find(e => e.type == 'text/qml');
 
-var qmlContent = qmlFile.innerHTML;
-console.log (qmlFile);
+var qmlContent = qmlFile.src? loadLocalText(qmlFile.src): qmlFile.innerHTML;
+console.log (qmlContent);
